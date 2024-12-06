d811ef931a41b77b82a98cd3c41a481d3036e632
JitendraPra40 <jitendra.pra40@gmail.com>
2024-12-06 20:57:22 +0530

    Resolved routing issues

--- server/server.py
+++ server/server.py
@@ -1,8 +1,18 @@
-from flask import Flask,request, jsonify
+import os
+from flask import Flask,request, jsonify, send_from_directory
 import util
 app = Flask(__name__)
 util.load_saved_artifacts()
 
+@app.route('/')
+def index():
+    client_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../client')
+    return send_from_directory(client_dir, 'app.html')
+
+@app.route('/<path:filename>')
+def static_files(filename):
+    return send_from_directory(os.path.join(os.path.dirname(__file__), '../client'), filename)
+    
 @app.route('/get_location_names', methods=['GET'])  # Default route
 def get_location_names():
     try:
